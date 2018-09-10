22e91fd03944388d5c8e1cf9202e79e0ef15c867
Moses Carayol <u1434206@uel.ac.uk>
2018-09-10 08:11:29 +0100

    Update Controller.php

--- webblog/app/Http/Controllers/Controller.php
+++ webblog/app/Http/Controllers/Controller.php
@@ -14,19 +14,22 @@ class Controller extends BaseController
   
     public $status;
     
-    public function isLoggedIn(){
+    public function isLoggedIn()
+    {
         $user_id = (null != Auth::user()) ? Auth::user()->id : ''; 
         $status = (null == $user_id) ? 0 : 1;
         $this->status = $status;               
     }
 
-    public function isOwnPost($post){
+    public function isOwnPost($post)
+    {
         $user_id = (null != Auth::user()) ? Auth::user()->id : '';
         
         return ($post[0]->user_id == $user_id) ? 1 : 0;        
     }    
 
-    public function setStatus($status){
+    public function setStatus($status)
+    {
          $this->status = $status;
     }
 }
